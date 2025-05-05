e70488379c38d7950c5e965e92dbee03dafff8ce
Janabh0 <janaalhamad21@gmail.com>
2025-05-05 19:33:39 +0300

    Solved 2

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,39 @@
+import products from "@/data/products";
 import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <>
-      <View style={{ paddingTop: 10, alignItems: "center" }}>
-        <Text style={{ fontWeight: "bold", fontSize: 20 }}>
-          Welcome to Book Cafe!
-        </Text>
-        <Text style={{ padding: 10 }}>We are a bookstore & cafe</Text>
-        <Image
-          source={{
-            uri: "https://coffeeshopstartups.com/wp-content/uploads/2015/04/Start-a-Coffee-Shop-Bookstore-Cafe-Bookstore.jpg",
-          }}
-          style={{ width: 300, height: 200, padding: 30 }}
-        />
+    <View style={{ paddingTop: 10, alignItems: "center" }}>
+      <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+        Welcome to Book Cafe!
+      </Text>
+      <Text style={{ padding: 10 }}>We are a bookstore & cafe</Text>
+      <Image
+        source={{
+          uri: "https://coffeeshopstartups.com/wp-content/uploads/2015/04/Start-a-Coffee-Shop-Bookstore-Cafe-Bookstore.jpg",
+        }}
+        style={{ width: 300, height: 200, padding: 30 }}
+      />
+
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <View style={{ margin: 10, alignItems: "center" }}>
+          <Image
+            source={{ uri: products[0].img }}
+            style={{ width: 140, height: 100, borderRadius: 10 }}
+          />
+          <Text style={{ marginTop: 5 }}>{products[0].name}</Text>
+          <Text>{products[0].price}</Text>
+        </View>
+
+        <View style={{ margin: 10, alignItems: "center" }}>
+          <Image
+            source={{ uri: products[1].img }}
+            style={{ width: 140, height: 100, borderRadius: 10 }}
+          />
+          <Text style={{ marginTop: 5 }}>{products[1].name}</Text>
+          <Text>{products[1].price}</Text>
+        </View>
       </View>
-    </>
+    </View>
   );
 }
---
+++ data/products.ts
@@ -0,0 +1,22 @@
+// /data/products.ts
+
+interface Product {
+  name: string;
+  price: string;
+  img: string;
+}
+
+const products: Product[] = [
+  {
+    name: "Cafe book",
+    price: "10",
+    img: "https://sweatpantsandcoffee.com/wp-content/uploads/2019/11/Housing-Works-Bookstore-Cafe-600x400.jpg",
+  },
+  {
+    name: "The book tea",
+    price: "19.99",
+    img: "https://cdn.vox-cdn.com/thumbor/T5giKYwnpXdOUpyN-Gr9SqDx6Oo=/0x0:1242x1512/1200x0/filters:focal(0x0:1242x1512):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/23224352/233563604_10159247165331638_8394971174235672003_n.jpeg",
+  },
+];
+
+export default products;

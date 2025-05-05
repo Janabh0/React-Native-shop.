28bca046c901bc0673d37818f5f06eca65df34d2
Janabh0 <janaalhamad21@gmail.com>
2025-05-05 18:14:37 +0300

    solved

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Image, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <>
+      <View style={{ paddingTop: 10, alignItems: "center" }}>
+        <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+          Welcome to Book Cafe!
+        </Text>
+        <Text style={{ padding: 10 }}>We are a bookstore & cafe</Text>
+        <Image
+          source={{
+            uri: "https://coffeeshopstartups.com/wp-content/uploads/2015/04/Start-a-Coffee-Shop-Bookstore-Cafe-Bookstore.jpg",
+          }}
+          style={{ width: 300, height: 200, padding: 30 }}
+        />
+      </View>
+    </>
+  );
+}
